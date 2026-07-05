bb27c9e28acbe8c36a54a83032e014f28af84bd5
srinivasBJ <your-email@example.com>
2026-07-05 08:54:06 +0530

    feat: install remark-math and rehype-katex to render mathematical equations and LaTeX formulas in articles

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
@@ -20,6 +22,8 @@ export default defineConfig({
   ],
 
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       themes: {
         light: 'github-light',
